eeefa93b9bc2aadd2af202339f4b4a08cb742843
Ted M. Young <tedyoung@gmail.com>
2022-05-13 11:22:57 -0700

    Ensemble #5: Continuing work on assigning IDs to Games through the incoming and redirection URLs

--- src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackControllerTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackControllerTest.java
@@ -4,16 +4,11 @@ import com.jitterted.ebp.blackjack.StubDeck;
 import com.jitterted.ebp.blackjack.application.GameIdGenerator;
 import com.jitterted.ebp.blackjack.application.GameNotFound;
 import com.jitterted.ebp.blackjack.application.GameService;
-import com.jitterted.ebp.blackjack.domain.Card;
 import com.jitterted.ebp.blackjack.domain.Deck;
 import com.jitterted.ebp.blackjack.domain.Game;
-import com.jitterted.ebp.blackjack.domain.Rank;
-import com.jitterted.ebp.blackjack.domain.Suit;
 import org.junit.jupiter.api.Test;
 import org.springframework.ui.ConcurrentModel;
 import org.springframework.ui.Model;
-
-import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -35,31 +30,22 @@ class BlackjackControllerTest {
     }
 
     @Test
-    public void gameViewPopulatesViewModelWithAllCards() throws Exception {
-        Deck stubDeck = new StubDeck(List.of(new Card(Suit.DIAMONDS, Rank.TEN),
-                                             new Card(Suit.HEARTS, Rank.TWO),
-                                             new Card(Suit.DIAMONDS, Rank.KING),
-                                             new Card(Suit.CLUBS, Rank.THREE)));
+    public void gameViewPopulatesViewModelWithGameViewInstanceAndGameId() throws Exception {
         GameIdGenerator gameIdGenerator = new GameIdGenerator(13L);
-        GameService gameService = new GameService(stubDeck, gameIdGenerator);
+        GameService gameService = new GameService(new Deck(), gameIdGenerator);
         BlackjackController blackjackController = new BlackjackController(gameService);
         blackjackController.startGame();
 
         Model model = new ConcurrentModel();
         blackjackController.gameView(model, 13L);
 
-        GameView gameView = (GameView) model.getAttribute("gameView");
-
-        assertThat(gameView.getDealerCards())
-                .containsExactly("2♥", "3♣");
-
-        assertThat(gameView.getPlayerCards())
-                .containsExactly("10♦", "K♦");
-
+        assertThat(model.getAttribute("gameView"))
+                .isInstanceOf(GameView.class);
         Long gameId = (Long) model.getAttribute("gameId");
         assertThat(gameId)
                 .isEqualTo(13L);
     }
+
 
     @Test
     public void hitCommandResultsInPlayerHavingThreeCardsAndPlayerIsNotDone() throws Exception {
@@ -73,10 +59,8 @@ class BlackjackControllerTest {
 
         assertThat(redirectPage)
                 .isEqualTo("redirect:/game/0");
-
         assertThat(game.playerHand().cards())
                 .hasSize(3);
-
         assertThat(game.isPlayerDone())
                 .isFalse();
     }
---
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/GameViewTest.java
@@ -0,0 +1,34 @@
+package com.jitterted.ebp.blackjack.adapter.in.web;
+
+import com.jitterted.ebp.blackjack.StubDeck;
+import com.jitterted.ebp.blackjack.domain.Card;
+import com.jitterted.ebp.blackjack.domain.Deck;
+import com.jitterted.ebp.blackjack.domain.Game;
+import com.jitterted.ebp.blackjack.domain.Rank;
+import com.jitterted.ebp.blackjack.domain.Suit;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class GameViewTest {
+
+    @Test
+    public void gameViewPopulatesViewModelWithAllCards() throws Exception {
+        Deck stubDeck = new StubDeck(List.of(new Card(Suit.DIAMONDS, Rank.TEN),
+                                             new Card(Suit.HEARTS, Rank.TWO),
+                                             new Card(Suit.DIAMONDS, Rank.KING),
+                                             new Card(Suit.CLUBS, Rank.THREE)));
+        Game game = new Game(stubDeck);
+        game.initialDeal();
+
+        GameView gameView = GameView.of(game);
+
+        assertThat(gameView.getDealerCards())
+                .containsExactly("2♥", "3♣");
+        assertThat(gameView.getPlayerCards())
+                .containsExactly("10♦", "K♦");
+    }
+
+}
