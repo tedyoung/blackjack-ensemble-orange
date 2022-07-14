738d243ec02c12a54228a914b932d3d81d51bebc
Ted M. Young <tedyoung@gmail.com>
2022-07-13 20:42:21 -0700

    Added Testcontainers to the BlackjackGameApplicationTests so it also uses Testcontainers instead of requiring a locally running postgres.

--- src/test/java/com/jitterted/ebp/blackjack/BlackjackGameApplicationTests.java
+++ src/test/java/com/jitterted/ebp/blackjack/BlackjackGameApplicationTests.java
@@ -3,9 +3,14 @@ package com.jitterted.ebp.blackjack;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
 @Tag("integration")
+@Testcontainers
+@SpringBootTest(properties = {
+        "spring.test.database.replace=NONE",
+        "spring.datasource.url=jdbc:tc:postgresql:14:///springboot"
+})
 class BlackjackGameApplicationTests {
 
     @Test
